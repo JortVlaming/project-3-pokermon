1e490d3ba8979df67e87b67a67e1c20a24c4bff4
JortVlaming <jort.vlaming@outlook.com>
2025-03-01 13:35:01 +0100

    [feature]: pygame renderer
    details:
    - Een renderer om het tekenen te versimpelen
    - tekst functies om tekst te tekenen
    - achtergrond kleur is veranderbaar

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import time
 import pygame
 
 from logger import *
+from src.renderer import Renderer
 
 info("Hello pokermon!")
 
@@ -12,10 +13,10 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-clock = pygame.time.Clock()
+renderer = Renderer(screen)
+renderer.set_background_color("Red")
 
 UPDATE_CAP = 1.0/60.0
-game_thread = None
 
 running = True
 
@@ -27,25 +28,17 @@ def update():
 
 def render():
     # TODO: render game
-    screen.fill("Red")
-
-    textSTR = "Hello pokermon!"
-    text = font.render(textSTR, True, (255, 255, 255))
-    textpos = text.get_rect(centerx=SCREEN_WIDTH/2-len(textSTR)/2, y=10)
-    screen.blit(text, textpos)
+    renderer.draw_text_x_centered("Hello pokermon!", 50)
 
 def run():
     global running
 
     should_render = False
-    firstTime = 0
     lastTime = time.time_ns() / 1_000_000_000.0
-    passedTime = 0
     unprocessedTime = 0
 
     frameTime = 0
     frames = 0
-    fps = 0
 
     while running:
         firstTime = time.time_ns() / 1_000_000_000.0
@@ -76,18 +69,20 @@ def run():
         if should_render:
             verbose("RENDER")
 
+            renderer.start_frame()
+
             render()
+
+            renderer.end_frame()
 
             frames += 1
 
             should_render = False
 
             pygame.display.flip()
-
-            pass
         else:
             time.sleep(1.0/1000.0)
 
-set_level(LogLevel.DEBUG)
+set_level(LogLevel.VERBOSE)
 
 run()
---
+++ src/renderer.py
@@ -0,0 +1,48 @@
+from typing import Tuple
+
+import pygame
+
+from src.logger import verbose
+
+
+class Renderer:
+    def __init__(self, screen: pygame.Surface):
+        self.screen = screen
+        self.font: pygame.font.Font|None = None
+        self.set_font(None, 64)
+        self.text_color = (255, 255, 255)
+        self.background_color = (0, 0, 0)
+
+    def set_font(self, font, size):
+        self.font = pygame.font.Font(font, size)
+
+    def set_text_color(self, color: Tuple[int, int, int]|str):
+        self.text_color = color
+
+    def set_background_color(self, color: Tuple[int, int, int]|str):
+        self.background_color = color
+
+    def start_frame(self):
+        verbose("[renderer] started a frame")
+        self.screen.fill(self.background_color)
+
+    def end_frame(self):
+        pygame.display.flip()
+        verbose("[renderer] finished a frame")
+
+    def draw_text(self, text: str, x: int, y: int, **kwargs):
+        text_surface = self.font.render(text, kwargs["antialias"] if "antialias" in kwargs else True, kwargs["color"] if "color" in kwargs else self.text_color)
+        if "centered" in kwargs:
+            if kwargs["centered"]:
+                text_pos = text_surface.get_rect(centerx=x, centery=y)
+            else:
+                text_pos = text_surface.get_rect(x=x, y=y)
+        else:
+            text_pos = text_surface.get_rect(x=x, y=y)
+        self.screen.blit(text_surface, text_pos)
+
+    def draw_text_x_centered(self, text: str, y: int, **kwargs):
+        w, h = self.screen.get_size()
+        x = int(w/2-len(text)/2)
+        kwargs["centered"] = True
+        self.draw_text(text, x, y, **kwargs)
