fcd3135439dd453929261ffc2cf082859640b8a4
JortVlaming <jort.vlaming@outlook.com>
2025-03-07 13:20:36 +0100

    [fix]: verplaats de al bestaande code naar een staat
    details:
    - maak een testState
    - verplaats al bestaande code naar de test state

--- src/main.py
+++ src/main.py
@@ -5,13 +5,9 @@ import pygame
 from src.engine.globals import Globals
 from src.engine.inputManager import InputManager
 from src.engine.logger import *
-from src.engine.objects.sprite import Sprite
-from src.engine.objects.square import Square
 from src.engine.renderer import Renderer
-from src.engine.ui.imageButton import ImageButton
-from src.engine.ui.textButton import TextButton
-from src.engine.ui.button import Button
-from src.pokemons.pokemons.froggo import Froggo
+from src.engine.state.stateMachine import StateMachine
+from src.states.testState import TestState
 
 info("Hello pokermon!")
 
@@ -32,55 +28,16 @@ UPDATE_CAP = 1.0/60.0
 running = True
 
 font = pygame.font.Font(None, 64)
-square = Square(100, 100, "Blue", 100, 100)
-sprite = Sprite(300, 300, "assets/test.jpg")
-
-froggo = Froggo(100,400)
-
-render_test = Square(0, 300, "Yellow", 0, 25)
-test_width = 0
-test_mode = 5
-
-def test_on_click(btn: Button):
-    info("BUTTON CLICK")
-
-test_button = TextButton(400, 200, 100, 100, "Yellow", "Test")
-test_button.set_on_click(test_on_click)
-
-test_button2 = ImageButton(550, 200, "assets/cards/ruit/ruitA.png", 4)
-test_button2.set_on_click(test_on_click)
-
-buttons = [test_button, test_button2]
 
 inputManager = InputManager()
+Globals.inputManager = inputManager
 
-def update():
-    global test_width, test_mode
-    # TODO: update game
-    test_width += test_mode
-    if test_width >= 800 and test_mode == 5:
-        test_mode = -5
-    elif test_width <= 0 and test_mode == -5:
-        test_mode = 5
+stateMachine = StateMachine()
+Globals.stateMachine = stateMachine
 
-    if inputManager.is_key_down(pygame.K_SPACE):
-        info("SPACE BAR PRESSED")
-    if inputManager.is_key_held(pygame.K_SPACE):
-        info("SPACE BAR HELD")
+testState = TestState()
 
-def render():
-    # TODO: render game
-    renderer.draw_text_x_centered("Hello pokermon!", 50)
-    square.draw()
-    sprite.draw()
-
-    render_test.set_width(test_width)
-    render_test.draw()
-
-    froggo.draw()
-
-    for btn in buttons:
-        btn.draw()
+stateMachine.huidige_staat = testState
 
 def run():
     global running
@@ -111,15 +68,10 @@ def run():
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    pos = pygame.mouse.get_pos()
-                    for btn in buttons:
-                        if btn.is_in_bounds(pos[0], pos[1]):
-                            btn.click()
 
                 inputManager.process_event(event)
 
-            update()
+            stateMachine.update()
 
             if frameTime >= 1.0:
                 frameTime = 0
@@ -130,7 +82,7 @@ def run():
         if should_render:
             renderer.start_frame()
 
-            render()
+            stateMachine.draw()
 
             renderer.end_frame()
 
