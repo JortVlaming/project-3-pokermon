07fbd530f3b673417c7517889cfc6a215c284b9e
JortVlaming <jort.vlaming@outlook.com>
2025-03-07 09:26:34 +0100

    [fix]: fix keyboard interrupt error
    details:
    - voeg try except toe voor error handling

--- run.py
+++ run.py
@@ -2,6 +2,9 @@ from src.main import run
 from src.engine.logger import set_level, LogLevel
 
 if __name__ == "__main__":
-    set_level(LogLevel.DEBUG)
+    try:
+        set_level(LogLevel.DEBUG)
 
-    run()
+        run()
+    except KeyboardInterrupt:
+        print("Game stopped by keyboard interrupt")
