442e261eb62acaa1bc325d77f8b5f64fdde34c67
Lars Sijbesma <183395@student.talland.nl>
2025-03-07 11:02:20 +0100

    [add]:State Machine [state machine finished]
    details:
    - state machine finished

--- src/engine/state/state.py
+++ src/engine/state/state.py
@@ -7,12 +7,3 @@
 
     def draw(self):
         pass
-
-class main(State):
-    def __init__(self):
-        State.__init__(self)
-
-    def update(self):
-        pass
-    def draw(self):
-        pass
