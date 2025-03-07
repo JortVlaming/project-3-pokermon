9f61af41010a9816341a68eff2973c8f46ffc497
Lars Sijbesma <183395@student.talland.nl>
2025-03-07 10:23:38 +0100

    [feature]: ["state class made"]
    details:
    - state class aangemaakt voor state machine

---
+++ src/engine/state/state.py
@@ -0,0 +1,18 @@
+﻿class State:
+    def __init__(self):
+        pass
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
+
+class main(State):
+    def __init__(self):
+        State.__init__(self)
+
+    def update(self):
+        pass
+    def draw(self):
+        pass
