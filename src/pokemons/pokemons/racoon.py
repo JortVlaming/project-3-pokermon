2af51c5490ae2b971f6809671adfe888a1186075
Lars Sijbesma <183395@student.talland.nl>
2025-03-13 12:19:56 +0100

    [Add]: [Racoon class]
    details:
    - Racoon class added

---
+++ src/pokemons/pokemons/racoon.py
@@ -0,0 +1,19 @@
+﻿import pygame.transform
+
+from src.pokemons.classes.pokemon import Pokemon
+from src.pokemons.attacks.lick import Lick
+
+
+class Racoon(Pokemon):
+    def __init__(self, x:int, y:int):
+        super().__init__(x, y, "assets/Racoon.png")
+
+        self.name = "Trash Panda"
+        self.max_hp = 80
+        self.hp = self.max_hp
+        self.speed = 110
+        self.attack = 130
+
+        self.moves = [(Lick(), 10, 10)]
+
+        self.image = pygame.transform.scale(self.image, (self.image.get_width()*2.5, self.image.get_height()*2.5))
