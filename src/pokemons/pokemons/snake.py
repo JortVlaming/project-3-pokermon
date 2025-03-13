89d5b9c12e99efb40a911f020b453ab85dbc596b
Lars Sijbesma <183395@student.talland.nl>
2025-03-13 15:44:04 +0100

    [Add]: [Snake class added]
    details:
    - beschrijving van is verandert

---
+++ src/pokemons/pokemons/snake.py
@@ -0,0 +1,19 @@
+﻿import pygame.transform
+
+from src.pokemons.classes.pokemon import Pokemon
+from src.pokemons.attacks.lick import Lick
+
+
+class Snake(Pokemon):
+    def __init__(self, x:int, y:int):
+        super().__init__(x, y, "assets/snek.png")
+
+        self.name = "Snek"
+        self.max_hp = 100
+        self.hp = self.max_hp
+        self.speed = 140
+        self.attack = 170
+
+        self.moves = [(Lick(), 10, 10)]
+
+        self.image = pygame.transform.scale(self.image, (self.image.get_width()*2.5, self.image.get_height()*2.5))
