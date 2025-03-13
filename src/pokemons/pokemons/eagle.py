9e628d65c65eb408f6018f8834133e1d9e145359
Lars Sijbesma <183395@student.talland.nl>
2025-03-13 12:17:52 +0100

    [Add]: [Eagle class]
    details:
    - Eagle class

---
+++ src/pokemons/pokemons/eagle.py
@@ -0,0 +1,19 @@
+﻿import pygame.transform
+
+from src.pokemons.classes.pokemon import Pokemon
+from src.pokemons.attacks.lick import Lick
+
+
+class Eagle(Pokemon):
+    def __init__(self, x:int, y:int):
+        super().__init__(x, y, "assets/AREND.png")
+
+        self.name = "'MERICA"
+        self.max_hp = 80
+        self.hp = self.max_hp
+        self.speed = 180
+        self.attack = 170
+
+        self.moves = [(Lick(), 10, 10)]
+
+        self.image = pygame.transform.scale(self.image, (self.image.get_width()*2.5, self.image.get_height()*2.5))
