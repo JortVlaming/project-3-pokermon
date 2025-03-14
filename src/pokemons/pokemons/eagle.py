8aebc84772ff110dfbfc16474a73a92133200745
Lars Sijbesma <183395@student.talland.nl>
2025-03-14 09:15:34 +0100

    [Add]: [Move added to Eagle]
    details:
    - beschrijving van is verandert

--- src/pokemons/pokemons/eagle.py
+++ src/pokemons/pokemons/eagle.py
@@ -1,7 +1,10 @@
 ﻿import pygame.transform
 
+from src.pokemons.attacks.gun import Gun
+from src.pokemons.attacks.steelWing import SteelWing
+from src.pokemons.attacks.windCutter import WindCutter
 from src.pokemons.classes.pokermon import Pokermon
-from src.pokemons.attacks.lick import Lick
+
 
 
 class Eagle(Pokermon):
@@ -14,6 +17,12 @@ class Eagle(Pokermon):
         self.speed = 180
         self.attack = 170
 
-        self.moves = [[Lick(), 10, 10]]
+        self.moves = [
+
+            [Gun(), 10, 10],
+            [WindCutter(), 10, 10],
+            [SteelWing(), 10, 10],
+
+        ]
 
         self.image = pygame.transform.scale(self.image, (self.image.get_width()*2.5, self.image.get_height()*2.5))
