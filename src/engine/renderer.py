58f4a752fd61d77a4d5ce392f4996f8dc1c58217
JortVlaming <jort.vlaming@outlook.com>
2025-03-19 10:51:23 +0100

    [refactor]: commentaar toegevoegd aan de renderer

--- src/engine/renderer.py
+++ src/engine/renderer.py
@@ -12,10 +12,22 @@ class Renderer:
         self.set_font(None, 64)
         self.text_color = (255, 255, 255)
 
-    def set_font(self, font, size):
+    def set_font(self, font: str | None, size:int):
+        """
+        Verandert de huidige font van de renderer
+        :param font: naam van de nieuwe font
+        :param size: de grootte van de nieuwe font
+        :return:
+        """
         self.font = pygame.font.Font(font, size)
 
     def set_text_color(self, color: Tuple[int, int, int]|str):
+        """
+        Verandert de huidige kleur van de tekst die wordt gerenderd.
+        Kleur wordt aangepast voor alle tekst die wordt geredeneerd na deze functie is aangeroepen.
+        :param color: De nieuwe tekst kleur
+        :return:
+        """
         self.text_color = color
 
     def start_frame(self):
@@ -32,24 +44,41 @@ class Renderer:
         verbose("[renderer] finished a frame")
 
     def get_font_of_size(self, size: int):
+        """
+        Krijg de standaard font in een specifieke grootte
+        :param size: De grootte van de font
+        :return: Font met een specifieke grootte
+        """
         return pygame.font.Font(None, size)
 
-    def draw_text(self, text: str, x: int, y: int, **kwargs):
-        f = self.font
-        if "size" in kwargs:
-            if isinstance(kwargs["size"], int):
-                f = self.get_font_of_size(kwargs["size"])
-        text_surface = f.render(text, kwargs["antialias"] if "antialias" in kwargs else True, kwargs["color"] if "color" in kwargs else self.text_color)
-        if "centered" in kwargs:
-            if kwargs["centered"]:
-                text_pos = text_surface.get_rect(centerx=x, centery=y)
-            else:
-                text_pos = text_surface.get_rect(x=x, y=y)
+    def draw_text(self, text: str, x: int, y: int, size:int=64, antialias:bool=True, color:str|Tuple[int, int, int]="Black", centered:bool=True):
+        """
+        Rendered tekst op het scherm op een specifieke positie
+        :param text: De tekst om te renderen op het scherm
+        :param x: De X positie van de tekst
+        :param y: De Y positie van de tekst
+        :param size: (optional: int) De grootte van de font om te gebruiken
+        :param antialias: (optional: True or False) Of de tekst moet worden gerenderd met antialiasing
+        :param color: (optional: str or Tuple[int, int, int]) Kleur van de tekst om te renderen
+        :param centered: (optional: bool) Of de tekst is gecentreerd op de X, Y positie
+        :return:
+        """
+        f = self.get_font_of_size(size)
+        text_surface = f.render(text, antialias, color)
+        if centered:
+            text_pos = text_surface.get_rect(centerx=x, centery=y)
         else:
             text_pos = text_surface.get_rect(x=x, y=y)
         self.screen.blit(text_surface, text_pos)
 
     def draw_text_x_centered(self, text: str, y: int, **kwargs):
+        """
+        Render tekst in het midden van het scherm.
+        :param text: De tekst om te renderen
+        :param y: De Y positie van de tekst
+        :param kwargs: Optionele argument (zie :funcThe Dukedom of the Barbarian Duke X, located north of the Burning Desert. :`Renderer.draw_text`)
+        :return:
+        """
         w, h = self.screen.get_size()
         x = int(w/2-len(text)/2)
         kwargs["centered"] = True
@@ -96,10 +125,28 @@ class Renderer:
         return self.font.render(txt, False, (0, 0, 0, 0)).get_width()
 
     def draw_rect(self, color: str | Tuple[int, int, int] | Tuple[int, int, int, int], x:int, y:int, width: int, height: int, border_radius:int=0):
+        """
+        Teken een vierkant op het scherm
+        :param color: De kleur van het vierkant
+        :param x: De X positie van het vierkant
+        :param y: De Y positie van het vierkant
+        :param width: De breedte van het vierkant
+        :param height: De hoogte van het vierkant
+        :param border_radius: De border-radius van het vierkant
+        :return:
+        """
         r = pygame.draw.rect(self.screen, color, pygame.Rect(x, y, width, height), border_radius=border_radius)
         return r
 
     def draw_image(self, image: str | pygame.Surface, x:int, y:int, image_scale:int=1):
+        """
+        Tekent een image op het scherm
+        :param image: Het pad naar de afbeelding of een al geladen image
+        :param x: De X positie van de afbeelding
+        :param y: De Y positie van de afbeelding
+        :param image_scale: Een size multiplier voor de afbeelding
+        :return:
+        """
         if isinstance(image, str):
             image = pygame.image.load(image)
             image = pygame.transform.scale(image, (image.get_width() * image_scale, image.get_height() * image_scale))
