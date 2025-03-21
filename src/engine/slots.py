b04402992bcff20e13197e0b344a7b149db52c1f
Koen <180389@student.talland.nl>
2025-03-21 09:15:56 +0100

    fix slots werkt!!!

--- src/engine/slots.py
+++ src/engine/slots.py
@@ -1,6 +1,9 @@
 import random
+from src.utils.input import int_input
+
 balance = 10
 winning = 0
+bet = 0
 
 
 # symbolen die gerolt kunnen worden
@@ -23,37 +26,35 @@ def win_amount(rolling):
     global winning
     global bet
     if rolling[0] == "cherry" or rolling[1] == "cherry" or rolling[2] == "cherry":
-        winning = bet * 1
-        balance += winning
-        print(f"You get: {winning} back and your balance is still : {balance}")
+        print(f"You get: {bet} back and your balance is still : {balance}")
         return balance, winning
     if rolling[0] == "cherry" and rolling[1] == "cherry" or rolling[2] == "cherry" and rolling[3] == "cherry" or rolling[0] == "cherry" and rolling[2] == "cherry" :
-        winning = bet * 3
+        winning = bet * 2
         balance += winning
         print(f"You won: {winning} and your balance is now: {balance}")
         return balance, winning
     if rolling[0] == "cherry" and rolling[1] == "cherry" and rolling[2] == "cherry":
-        winning = bet * 4
+        winning = bet * 3
         balance += winning
         print(f"You won: {winning} and your balance is now: {balance}")
         return balance, winning
     if rolling[0] == "lemon" and rolling[1] == "lemon" and rolling[2] == "lemon":
-        winning = bet * 5
+        winning = bet * 4
         balance += winning
         print(f"You won: {winning} and your balance is now: {balance}")
         return balance, winning
     if rolling[0] == "grapes" and rolling[1] == "grapes" and rolling[2] == "grapes":
-        winning = bet * 6
+        winning = bet * 5
         balance += winning
         print(f"You won: {winning} and your balance is now: {balance}")
         return balance, winning
     if rolling[0] == "watermelon" and rolling[1] == "watermelon" and rolling[2] == "watermelon":
-        winning = bet * 7
+        winning = bet * 6
         balance += winning
         print(f"You won: {winning} and your balance is now: {balance}")
         return balance, winning
     if rolling[0] == "seven" and rolling[1] == "seven" and rolling[2] == "seven":
-        winning = bet * 8
+        winning = bet * 7
         balance += winning
         return balance, winning
     else:
@@ -63,27 +64,24 @@ def win_amount(rolling):
 
 def main():
     global bet
+    global balance
     print("Welcome to the Slots Game!")
-    while balance >= 0:
-        try:
-            bet = int(input("Enter bet: "))
-            break
-        except:
-            print("Sorry, that's not a valid number.")
-
-        if bet <= 0:
-            print("Sorry, you cant bet nothing")
-
-        elif bet > balance:
+    print(f"youre balance is now: {balance}")
+    while balance > 1:
+        bet = int_input("Enter bet: ")
+        if bet > balance:
             print("Sorry, you don't have enough money!")
-        else:
-            input("press enter to spin the slots")
-            rols = rolling()
-            print(f"slot 1: {rols[0]}, slot 2: {rols[1]}, slot 3: {rols[2]}")
-            win = win_amount(rols)
 
-            play_again = input("Do you want to play again? (yes/no): ").lower()
-            if play_again != "yes":
-                print("Thanks for playing! Goodbye!")
-                break
+        input("press enter to spin the slots")
+        rols = rolling()
+        print(f"slot 1: {rols[0]}, slot 2: {rols[1]}, slot 3: {rols[2]}")
+        win_amount(rolling())
+
+        play_again = input("Do you want to play again? (yes/no): ").lower()
+        if play_again != "yes":
+            print("Thanks for playing! Goodbye!")
+            break
+        elif play_again == "yes" and balance == 0:
+            print("Sorry, you don't have enough money left")
+            break
 main()
