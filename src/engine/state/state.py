ea2920ce7963e0eb2d5df9206db75c966de2332b
JortVlaming <jort.vlaming@outlook.com>
2025-03-29 11:39:19 +0100

    [feature]: blackjack klaar
    Signed-off-by: JortVlaming <jort.vlaming@outlook.com>

--- src/engine/state/state.py
+++ src/engine/state/state.py
@@ -27,5 +27,5 @@ class State:
         if inputManager.is_button_down(pygame.BUTTON_LEFT):
             mX, mY = pygame.mouse.get_pos()
             for btn in self.buttons:
-                if btn.is_in_bounds(mX, mY) and btn.do_clicks:
+                if btn.is_in_bounds(mX, mY) and btn.do_clicks and btn.do_render:
                     btn.click()
