3c7e66e518f669b86ebe07e8403f4eae66f0136c
JortVlaming <jort.vlaming@outlook.com>
2025-04-06 20:37:57 +0200

    [fix]: whooper naam te lang lmao

--- src/pokemons/pokemons/whooper.py
+++ src/pokemons/pokemons/whooper.py
@@ -11,7 +11,7 @@ class Whooper(Pokermon):
     def __init__(self):
         super().__init__("assets/Energyzwordedwopper.png")
 
-        self.name = "Whoooooooooooo"
+        self.name = "Whooooooooooo"
         self.max_hp = 145
         self.hp = self.max_hp
         self.speed = 90
